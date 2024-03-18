73d45d12bee485cbded4782c618877fa3405efee
aligilim <aligilim@hotmail.com>
2024-03-18 18:56:59 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from textblob import Word, TextBlob
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from nltk.sentiment import SentimentIntensityAnalyzer
+from warnings import filterwarnings
+
+filterwarnings('ignore')
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+pd.set_option('display.width', 200)
+
+
+df = pd.read_excel("amazon.xlsx")
+
+df.head()
+df.info()
+
+# normalizing
+df['Review'] = df['Review'].str.lower()
+
+# punctuations
+df['Review'] = df['Review'].str.replace('[^\w\s]', '')
+
+# number
+df['Review'] = df['Review'].str.replace('\d', '')
+
+# stopwords
+#nltk.download('stopwords')
+sw = stopwords.words('english')
+df['Review'] = df['Review'].apply(lambda x: " ".join(x for x in str(x).split() if x not in sw))
+
+
+drop = pd.Series(' '.join(df['Review']).split()).value_counts()[-1000:]
+df['Review'] = df['Review'].apply(lambda x: " ".join(x for x in x.split() if x not in drop))
+
+#lemmatization
+nltk.download('wordnet')
+df['Review'] = df['Review'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+df['Review'].head(10)
+
+
+nltk.download('vader_lexicon')
+sia = SentimentIntensityAnalyzer()
+
+df["Review"][0:10].apply(lambda x: sia.polarity_scores(x))
+#compound score for first 10 review
+df["Review"][0:10].apply(lambda x: sia.polarity_scores(x)["compound"])
+
+df["Review"][0:10].apply(lambda x: "pos" if sia.polarity_scores(x)["compound"] > 0 else "neg")
+
+df["Sentiment_Label"] = df["Review"].apply(lambda x: "pos" if sia.polarity_scores(x)["compound"] > 0 else "neg")
+
+df.groupby("Sentiment_Label")["Star"].mean()
+
+
+train_x, test_x, train_y, test_y = train_test_split(df["Review"],
+                                                    df["Sentiment_Label"],
+                                                    random_state=42)
+
+tf_idf_word_vectorizer = TfidfVectorizer().fit(train_x)
+x_train_tf_idf_word = tf_idf_word_vectorizer.transform(train_x)
+x_test_tf_idf_word = tf_idf_word_vectorizer.transform(test_x)
+
+log_model = LogisticRegression().fit(x_train_tf_idf_word, train_y)
+
+y_pred = log_model.predict(x_test_tf_idf_word)
+
+print(classification_report(y_pred, test_y))
+
+cross_val_score(log_model, x_test_tf_idf_word, test_y, cv=5).mean()
+
+#Randomly selecting comments from the data and asking the model.
+random_review = pd.Series(df["Review"].sample(1).values)
+yeni_yorum = CountVectorizer().fit(train_x).transform(random_review)
+pred = log_model.predict(yeni_yorum)
+print(f'Review:  {random_review[0]} \n Prediction: {pred}')
+
+#random forest
+rf_model = RandomForestClassifier().fit(x_train_tf_idf_word, train_y)
+cross_val_score(rf_model, x_test_tf_idf_word, test_y, cv=5, n_jobs=-1).mean()
+
+
